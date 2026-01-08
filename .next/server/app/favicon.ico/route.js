532e98ae9bf82c0e56d79a14a4f987b25873cece
romanvish <97044940+romanvish@users.noreply.github.com>
2026-01-08 12:35:05 -0700

    Delete .next directory

--- .next/server/app/favicon.ico/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
-R.c("server/chunks/8b612_next_0d845a12._.js")
-R.c("server/chunks/[root-of-the-server]__bc84fa5c._.js")
-R.m("[project]/EPICenter-Website/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/EPICenter-Website/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/EPICenter-Website/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/EPICenter-Website/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/EPICenter-Website/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/static/chunks/EPICenter-Website_src_app_page_tsx_d796fc70._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/8b612_next_dist_a3d49421._.js"
-],
-    source: "dynamic"
-});

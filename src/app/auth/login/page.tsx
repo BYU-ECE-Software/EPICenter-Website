39624898be548fae73acdde1afdfde25af2a3b58
romanvish <97044940+romanvish@users.noreply.github.com>
2026-01-08 12:36:22 -0700

    Delete src directory

--- src/app/auth/login/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div className="text-center p-4">Please Sign In with your BYU Account</div>
-  );
-}
--- src/app/auth/logout/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function LogoutPage() {
-  return <div className="text-center p-4">You have been signed out.</div>;
-}
